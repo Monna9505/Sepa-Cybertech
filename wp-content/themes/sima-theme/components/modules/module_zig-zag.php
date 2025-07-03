7eef999911068a3a706973c1904c7a3e7c6d39d4
denitsa_mitkova <denitsa@orbisds.com>
2025-07-03 18:09:47 +0300

    Zig Zag Module is done

---
+++ wp-content/themes/sima-theme/components/modules/module_zig-zag.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Module Zig Zag
+ */
+if (!isset($module)) { return; }
+
+$zig_zag = (isset($module['zig_zag_repeater'])) ? $module['zig_zag_repeater'] : false; //Repeater
+
+?>
+
+<div class="zig_zag_component">
+    <?php foreach($zig_zag as $item) { ?>
+        <div class="bg_image" style="background-image: url(<?php echo (!empty($item['background_image']) && isset($item['background_image']['url'])) ? $item['background_image']['url'] : ""; ?>);">
+            <div class="container">
+                <div class="item_wrapper <?php echo $item['select_position_image']?>">
+                    <div class="img_wrapper">
+                        <?php if (isset($item['image']) && !empty($item['image'])) { ?>
+                                <img src="<?php echo $item['image']['url']; ?>" 
+                                    alt="<?php echo $item['image']['title']; ?>">
+                                <div class="tear__shape__dark__blue"></div>
+                        <?php } ?>
+                    </div>
+                    <div class="text_wrapper">
+                        <?php if (isset($item['main_title']) && !empty($item['main_title'])) { ?>
+                            <div class="main_title">
+                                <h2><?php echo $item['main_title']; ?></h2>
+                            </div>
+                        <?php } ?>
+                        <?php if (isset($item['description']) && !empty($item['description'])) { ?>
+                            <div class="descr">
+                                <p><?php echo $item['description']; ?></p> 
+                            </div>
+                        <?php } ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+    <?php } ?>
+</div>
